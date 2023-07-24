b7637a8e9dfec83fb8f43959db1eb5078e4c6a8c
fastoch <fastoch@ik.me>
2023-07-24 20:59:31 +0200

    *

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// Rock, Paper, Scissors: refactored with a While Loop & Arrays
+
+window.onload = function game() {
+  let playGame = confirm("Shall we play rock, paper, or scissors?");
+  if (playGame) {
+    while (playGame) {
+      const playerChoice = prompt("Please enter rock, paper, or scissors:");
+      if (playerChoice || playerChoice === "") {
+        const playerOne = playerChoice.trim().toLowerCase();
+        if (
+          playerOne === "rock" ||
+          playerOne === "paper" ||
+          playerOne === "scissors"
+        ) {
+          const computerChoice = Math.floor(Math.random() * 3 + 1);
+          const rpsArray = ["rock", "paper", "scissors"];
+          const computer = computerChoice === 1 ? "rock" : computerChoice === 2 ? "paper" : "scissors";
+
+          const result =
+            playerOne === computer
+              ? "Tie game!"
+              : playerOne === "rock" && computer === "paper"
+                ? `player: ${playerOne}\ncomputer: ${computer}\nComputer wins!`
+                : playerOne === "paper" && computer === "scissors"
+                  ? `player: ${playerOne}\ncomputer: ${computer}\nComputer wins!`
+                  : playerOne === "scissors" && computer === "rock"
+                    ? `player: ${playerOne}\ncomputer: ${computer}\nComputer wins!`
+                    : `player: ${playerOne}\ncomputer: ${computer}\nPlayer wins!`;
+          alert(result);
+
+          playGame = confirm("Would you like to play again?");
+          if (!playGame) alert("Ok, thanks for playing.");
+          continue;
+        } else {
+          alert("You didn't enter rock, paper, or scissors.");
+          continue;
+        }
+      } else {
+        alert("I guess you changed your mind. Maybe next time...");
+      }
+    }
+  }
+}
